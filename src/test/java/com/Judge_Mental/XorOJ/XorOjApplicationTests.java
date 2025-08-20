975e6038f7c8a70ec365969f2b5567648588d08a
Nayeem10 <nayeemhossain2110@gmail.com>
2025-08-20 11:03:56 +0600

    first commit

---
+++ src/test/java/com/Judge_Mental/XorOJ/XorOjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Judge_Mental.XorOJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XorOjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
